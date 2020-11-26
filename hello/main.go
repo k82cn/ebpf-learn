8163058176e41f84d7b430b2ef85a21b7908cbaf
ubuntu <ubuntu@localhost.localdomain>
2020-11-26 20:38:45 +0800

    init

---
+++ hello/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/iovisor/gobpf/elf"
+)
+
+func main() {
+	mod := elf.NewModule("hello/hello.o")
+
+	err := mod.Load(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	err = mod.EnableKprobes(0)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		fmt.Println("Waiting...")
+		time.Sleep(10 * time.Second)
+	}
+}
